b4d5b48e3ab3d0e7e3ec913afc3c98389ad201b8
Anya Gessesse <anyagess@fb.com>
2020-07-13 14:02:32 -0700

    search, home and profile fragments created

--- app/src/main/java/com/example/friendly/MainActivity.java
+++ app/src/main/java/com/example/friendly/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.friendly;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.friendly.fragments.HomeFragment;
+import com.example.friendly.fragments.ProfileFragment;
+import com.example.friendly.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_post_status:
+                        //TODO change to post status activity
+                        Toast.makeText(MainActivity.this, "post status clicked", Toast.LENGTH_SHORT).show();
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_search:
+                        fragment = new SearchFragment();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        //sets the default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
