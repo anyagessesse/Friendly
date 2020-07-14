8856951b5d9c6df8c1ae7c109dd169c1b8bce893
Anya Gessesse <anyagess@fb.com>
2020-07-14 10:48:57 -0700

    created a base for the profile fragment

--- app/src/main/java/com/example/friendly/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/friendly/fragments/ProfileFragment.java
@@ -9,13 +9,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.friendly.R;
+import com.parse.ParseUser;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+
+    private TextView tvUser;
+    private ImageView ivProfilePic;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -31,5 +37,12 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        tvUser = view.findViewById(R.id.tvUser);
+        ivProfilePic = view.findViewById(R.id.ivProfilePic);
+
+        ivProfilePic.setVisibility(View.GONE); //TODO make picture visible when there is a profile picture to show
+
+        tvUser.setText(ParseUser.getCurrentUser().getUsername());
     }
 }
