815db9a5e78be6ba6ca7503cefd8016b3462ca47
Anya Gessesse <anyagess@fb.com>
2020-07-15 08:44:45 -0700

    profile pictures on profile page

--- app/src/main/java/com/example/friendly/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/friendly/fragments/ProfileFragment.java
@@ -1,17 +1,24 @@
 package com.example.friendly.fragments;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.friendly.R;
 import com.parse.ParseUser;
 
@@ -19,6 +26,7 @@ import com.parse.ParseUser;
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+    public static final String TAG = "ProfileFragment";
 
     private TextView tvUser;
     private ImageView ivProfilePic;
@@ -43,7 +51,25 @@ public class ProfileFragment extends Fragment {
 
         //put user details into profile layout
         ivProfilePic.setVisibility(View.GONE); //TODO make picture visible when there is a profile picture to show
-        tvUser.setText(ParseUser.getCurrentUser().getUsername());
+
+        ParseUser curUser = ParseUser.getCurrentUser();
+        if(curUser.getParseFile("profilePic")!=null){
+            Glide.with(view).load(curUser.getParseFile("profilePic").getUrl()).listener(new RequestListener<Drawable>() {
+                @Override
+                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                    Log.e(TAG,"image load unsuccesful",e);
+                    return false;
+                }
+
+                @Override
+                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                    return false;
+                }
+            }).into(ivProfilePic);
+            ivProfilePic.setVisibility(View.VISIBLE);
+        }
+
+        tvUser.setText(curUser.getUsername());
         //TODO add recyclerview of friends
     }
 }
