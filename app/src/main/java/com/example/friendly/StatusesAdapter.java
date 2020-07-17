18e5b35ef2ac89890b03e931bb97e3cf07589c7d
Anya Gessesse <anyagess@fb.com>
2020-07-17 09:12:11 -0700

    slightly changed relative time stamp

--- app/src/main/java/com/example/friendly/StatusesAdapter.java
+++ app/src/main/java/com/example/friendly/StatusesAdapter.java
@@ -3,6 +3,7 @@ package com.example.friendly;
 import android.content.Context;
 import android.content.Intent;
 import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +28,11 @@ public class StatusesAdapter extends RecyclerView.Adapter<StatusesAdapter.ViewHo
 
     private final Context context;
     private final List<Status> statuses;
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+    public static final String TAG = "StatusesAdapter";
 
     public StatusesAdapter(Context context, List<Status> statuses) {
         this.context = context;
@@ -119,21 +125,30 @@ public class StatusesAdapter extends RecyclerView.Adapter<StatusesAdapter.ViewHo
         notifyDataSetChanged();
     }
 
-    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
     public String getRelativeTimeAgo(String rawJsonDate) {
         String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
         SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
         sf.setLenient(true);
 
-        String relativeDate = "";
         try {
-            long dateMillis = sf.parse(rawJsonDate).getTime();
-            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
-                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+            long time = sf.parse(rawJsonDate).getTime();
+            long now = System.currentTimeMillis();
+
+            final long diff = now - time;
+            if (diff < MINUTE_MILLIS) {
+                return "just now";
+            } else if (diff < 50 * MINUTE_MILLIS) {
+                return diff / MINUTE_MILLIS + " m";
+            } else if (diff < 24 * HOUR_MILLIS) {
+                return diff / HOUR_MILLIS + " h";
+            } else {
+                return diff / DAY_MILLIS + " d";
+            }
         } catch (ParseException e) {
+            Log.i(TAG, "getRelativeTimeAgo failed");
             e.printStackTrace();
         }
 
-        return relativeDate;
+        return "";
     }
 }
