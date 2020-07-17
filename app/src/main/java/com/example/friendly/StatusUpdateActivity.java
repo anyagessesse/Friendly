6ba0527c223414a0c080e5b3efcf46988dc191c9
Anya Gessesse <anyagess@fb.com>
2020-07-17 11:45:39 -0700

    added place autocomplete fragment

--- app/src/main/java/com/example/friendly/StatusUpdateActivity.java
+++ app/src/main/java/com/example/friendly/StatusUpdateActivity.java
@@ -8,13 +8,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.friendly.objects.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -28,6 +35,7 @@ public class StatusUpdateActivity extends AppCompatActivity {
     private Button postStatusButton;
     private Status newStatus;
     private TimePicker timePicker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +63,34 @@ public class StatusUpdateActivity extends AppCompatActivity {
                 Intent intent = new Intent(StatusUpdateActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        //user can search for a location to set in their status
+        //initializes google places api key
+        String apiKey = getString(R.string.google_api_key);
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), apiKey);
+        }
+        // Create a new Places client instance.
+        PlacesClient placesClient = Places.createClient(this);
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            }
+
+            @Override
+            public void onError(@NonNull com.google.android.gms.common.api.Status status) {
+                Log.i(TAG, "An error occurred: " + status);
             }
         });
     }
