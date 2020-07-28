4f9031192141e4538bb7f1526686509ade544947
Anya Gessesse <anyagess@fb.com>
2020-07-28 11:15:46 -0700

    user can delete their status

--- app/src/main/java/com/example/friendly/StatusDetailActivity.java
+++ app/src/main/java/com/example/friendly/StatusDetailActivity.java
@@ -1,14 +1,20 @@
 package com.example.friendly;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.friendly.objects.Status;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -26,6 +32,7 @@ public class StatusDetailActivity extends AppCompatActivity {
     private Status status;
     private TextView timeRange;
     private TextView location;
+    private Button deleteStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,7 @@ public class StatusDetailActivity extends AppCompatActivity {
         date = findViewById(R.id.text_date);
         timeRange = findViewById(R.id.text_time_range);
         location = findViewById(R.id.text_location);
+        deleteStatus = findViewById(R.id.delete_status);
 
         //get status clicked on
         status = (Status) getIntent().getParcelableExtra("status");
@@ -69,6 +77,41 @@ public class StatusDetailActivity extends AppCompatActivity {
             String state = status.getString("state");
             location.setVisibility(View.VISIBLE);
             location.setText(getString(R.string.location, city, state));
+        }
+
+        // show delete button if the status is of the logged in user
+        if (status.getUser().getObjectId().equals(ParseUser.getCurrentUser().getObjectId())) {
+            deleteStatus.setVisibility(View.VISIBLE);
+            deleteStatus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // launch dialog to confirm deleting the status
+                    AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                    builder.setMessage(getString(R.string.confirm_delete))
+                            .setCancelable(false)
+                            .setPositiveButton(getString(R.string.confirm), new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    // deleted the status and navigate back to main
+                                    try {
+                                        status.delete();
+                                        status.saveInBackground();
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    Intent intent = new Intent(StatusDetailActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            })
+                            .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+            });
         }
     }
 }
